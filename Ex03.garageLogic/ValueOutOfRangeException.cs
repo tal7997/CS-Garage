c73f3eee208bc3fb0f01b588b4730333b170c7bb
tal7997 <125656345+tal7997@users.noreply.github.com>
2024-02-24 18:13:41 +0200

    init first class on Garage project

---
+++ Ex03.garageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex03.garageLogic
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        private float m_MaxValue;
+        private float m_MinValue;
+
+        public ValueOutOfRangeException(float i_MaxValue, float i_MinValue, string i_Message) : base(i_Message)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
+    }
+}
---
+++ Ex03.garageLogic/Wheel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.garageLogic
+{
+    internal class Wheel
+    {
+        private string m_ManuFacturerName;
+        private float  m_CurrentAirPressure;
+        private float  m_MaxAirPressure;
+
+        public string ManuFacturerName 
+        {
+            get { return m_ManuFacturerName; }
+            set 
+            {
+                if ((value.ToString())[0] == ' ')
+                {
+                    throw new Exception("Invalid Name!\n\n");
+                }
+
+                m_ManuFacturerName = value;
+            }
+        }
+
+        public float CurrentAirPressure
+        {
+            get { return m_CurrentAirPressure; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ValueOutOfRangeException(m_MaxAirPressure, 0, "The value is out of range, the amount of air pressure must be positive.\n\n");
+                }
+
+                if (value > m_MaxAirPressure)
+                {
+                    throw new ValueOutOfRangeException(m_MaxAirPressure, 0, $"The amount of the air pressure can not exceed {m_MaxAirPressure}\n\n");
+                }
+
+                m_CurrentAirPressure = value;
+            }
+        }
+
+        public float MaxAirPressure
+        {
+            get { return m_MaxAirPressure; }
+        }
+
+        public Wheel(string i_ManufacturerName, float i_CurrentAirPressure, float i_MaxAirPressure)
+        {
+            m_MaxAirPressure = i_MaxAirPressure;
+            ManuFacturerName = i_ManufacturerName;
+            CurrentAirPressure = i_CurrentAirPressure;
+        }
+
+        public void Inflate(float airPressure)
+        {
+            //add check range
+            CurrentAirPressure += airPressure;
+        }
+
+        public override string ToString()
+        {
+            string message = string.Format($@"
+Manufacturer Name: {m_ManuFacturerName}
+Current Air Pressure: {m_CurrentAirPressure}
+Max Air Pressure: {m_MaxAirPressure}
+
+");
+            return message;
+        }
+    }
+}
